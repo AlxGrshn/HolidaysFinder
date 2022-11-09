b69cd415e988fd86d543c7afe0513aa5fec23565
AlxGrshn <113042128+AlxGrshn@users.noreply.github.com>
2022-11-09 19:15:15 +0300

    added logic for change image depending on the holiday

--- HolidaysFinder/Views/HolidayCell.swift
+++ HolidaysFinder/Views/HolidayCell.swift
@@ -17,6 +17,33 @@ class HolidayCell: UITableViewCell {
         nameLabel.text = holiday.name
         localNameLabel.text = holiday.localName
         dataLabel.text = holiday.date
-        holidayImage.image = UIImage(named: "ballons")
+        holidayImage.image = UIImage(named: getImage(with: holiday.name))
+    }
+}
+
+extension HolidayCell {
+    private func getImage(with name: String) -> String {
+        switch name {
+        case "New Year\'s Day":
+            return "newYear"
+        case "New Year holiday":
+            return "newYearHolidays"
+        case "Orthodox Christmas Day":
+            return "oldNewYear"
+        case "Defender of the Fatherland Day":
+            return "defenderDay"
+        case "International Women\'s Day":
+            return "womenDay"
+        case "Labour Day":
+            return "labourDay"
+        case "Victory Day":
+            return "victoryDay"
+        case "Russia Day":
+            return "russiaDay"
+        case "Unity Day":
+            return "unityDay"
+        default:
+            return "ballons"
+        }
     }
 }
